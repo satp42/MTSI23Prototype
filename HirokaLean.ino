94560c404ff9e1b75a35a8573eba568088e76d8e
satp42 <msg4satwik@gmail.com>
2023-07-22 16:15:18 -0400

    Added accelerometer and ultrasonic sensor dynamics

--- HirokaLean.ino
+++ HirokaLean.ino
@@ -18,6 +18,12 @@ float tiltThreshold = 1.3;
 unsigned long drinkTime = 2000;
 unsigned long drinkStart = 0;
 
+int distance = 0;
+
+// Define the trigger and echo pin of ultrasonic sensor
+#define TRIGGER_PIN 2   // You can change it to any other suitable Digital IO pin
+#define ECHO_PIN 3      // You can change it to any other suitable Digital IO pin
+
 void setup() {
   Serial.begin(9600);
 
@@ -25,27 +31,44 @@ void setup() {
   Blynk.begin(auth, ssid, pass);
 
   if(!accel.begin()) {
-    Serial.println("no ADXL345 detected... Check wiring");
+    Serial.println("no ADXL detected... Check wiring");
     while(1);
   }
+
+  // Initialize ultrasonic sensor pins
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
 
 void loop() {
-  sensors_event_t event;
-  accel.getEvent(&event);
+  // Sensing the orientation of water bottle using ultrasonic sensor
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  distance = pulseIn(ECHO_PIN, HIGH) / 58;   //58 is used for converting the sensed signal to cm
+  
+  // Assuming that water is sensed only when distance is less than 15 cm
+  // The constant 15 cm should be replaced by the actual height of your water bottle
+  if (distance < 15) {
+    // When the bottle is standing, start sensing the acceleration
+    sensors_event_t event;
+    accel.getEvent(&event);
 
-  if(event.acceleration.x > tiltThreshold || event.accleration.y > tiltThreshold) {
-    if (!isDrinking) {
-      drinkStart = millis();
-      isDrinking = true;
-    }
-    else if (isDrinking && (millis() - drinkStart >= drinkTime)) {
-      drinkCount++;
-      Blynk.virtualWrite(V0, drinkCount);
+    if(event.acceleration.x > tiltThreshold || event.accleration.y > tiltThreshold) {
+      if (!isDrinking) {
+        drinkStart = millis();
+        isDrinking = true;
+      }
+      else if (isDrinking && (millis() - drinkStart >= drinkTime)) {
+        drinkCount++;
+        Blynk.virtualWrite(V0, drinkCount);
+        isDrinking = false;
+      }
+    } else {
       isDrinking = false;
     }
-  } else {
-    isDrinking = false;
   }
 
   Blynk.run();
